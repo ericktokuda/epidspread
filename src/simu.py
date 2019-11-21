515c7e62853069a3935f81874634cb79cd80a11e
tokudaek <tokudaek@gmail.com>
2019-11-21 15:08:34 -0300

    Include processing machine in the summary

--- src/simu.py
+++ src/simu.py
@@ -362,10 +362,12 @@ def run_experiment(cfg):
     elapsed = time.time() - t0
     info('exp:{} Elapsed time: {:.2f}h'.format(expidx, elapsed/3600))
     summary = dict(
-        elapsed = str(elapsed),
+        server = socket.gethostname(),
+        elapsed = '{:.2f}'.format(elapsed),
         nsteps = lastepoch,
         stepsmobility = steps_mobility,
     )
+    print(elapsed)
     with open(summarypath, 'w') as fh:
         fh.write(','.join(summary.keys()) + '\n')
         fh.write(','.join(str(x) for x in summary.values()))
@@ -609,7 +611,7 @@ def generate_params_combinations(origcfg):
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('config', help='Config file')
-    parser.add_argument('--continue_', action='store_true', help='Continue experiments')
+    parser.add_argument('--overwrite', action='store_true', help='Overwrite')
     parser.add_argument('--shuffle', action='store_true', help='Shuffled traversing of config parameters')
     parser.add_argument('--usedhashes', default=None, help='Already used hashes')
     args = parser.parse_args()
@@ -621,11 +623,11 @@ def main():
 
     outdir = cfg.outdir[0]
 
-    # info('Files will be generated in {}/...'.format(outdir))
+    info('Files will be generated in {}/...'.format(outdir))
     existing = os.path.exists(outdir)
 
-    if existing and not args.continue_:
-        print('Folder {} exists. Change the outdir parameter or use --continue_'. \
+    if existing and not args.overwrite:
+        print('Folder {} exists. Change the outdir parameter or use --overwrite'. \
               format(outdir))
         return
 
@@ -637,11 +639,9 @@ def main():
     cfgkeys = list(cfg.index)
 
     if existing and os.path.exists(expspath): # Load config from exps
-        info('Ignoring {}. Loading existing {}/exps.csv'.format(args.config, outdir))
         df = pd.read_csv(expspath)
         params = df.to_dict(orient='records')
     else:
-        info('Loading {}'.format(args.config))
         if args.usedhashes: hashes = open(args.usedhashes).read().splitlines()
         else: hashes = []
 
