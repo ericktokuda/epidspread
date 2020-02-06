e8ab79a966de621d108873530ffcedb3388c1621
tokudaek <tokudaek@gmail.com>
2020-02-06 07:42:09 -0300

    change default name of the waxman buffer

--- src/simu.py
+++ src/simu.py
@@ -195,8 +195,8 @@ def generate_graph(topologymodel, nvertices, avgdegree,
         radius = get_rgg_params(nvertices, avgdegree)
         g = igraph.Graph.GRG(nvertices, radius)
     elif topologymodel == 'wx':
-        bufwaxmanpath = pjoin(tmpdir, 'waxman_{:01.4f}_{:02d}.pkl'.\
-                              format(wxalpha,randomseed))
+        bufwaxmanpath = pjoin(tmpdir, 'waxman_{:02d}_{:01.4f}_{:02d}.pkl'.\
+                              format(avgdegree, wxalpha,randomseed))
         try:
             with open(bufwaxmanpath, 'rb') as fh:
                 g = pkl.load(fh)
