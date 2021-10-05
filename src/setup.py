32f972be14d032a94dc31ae656e139510ff3f823
tokudaek@brucutuvi <tokudaek@gmail.com>
2021-10-04 21:41:57 -0300

    Fix cython compilation (add include_dirs=numpy)

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,8 @@
+import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("src/optimized.pyx", compiler_directives={'language_level' : "3"})
+    ext_modules = cythonize("src/optimized.pyx", compiler_directives={'language_level' : "3"}),
+    include_dirs=[numpy.get_include()]
 )
